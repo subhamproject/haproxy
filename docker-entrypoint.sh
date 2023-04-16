9f2a9e383ba1f4e295ddcd70a8ee46d4a0c934d0
Subham Mandal <subhammandal01@gmail.com>
2023-04-16 13:41:14 +0530

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ -n "$SERVER_CERT" ]
+then
+    echo "Writing certificate to /certs/rcx-server.pem"
+    umask 0022
+    mkdir /certs
+    echo $SERVER_CERT | grep -q ^PARM:
+    if [ $? -eq 0 ]
+    then
+        SERVER_CERT=`echo $SERVER_CERT | sed 's/^PARM://'`
+        aws ssm get-parameter --name $SERVER_CERT --with-decryption --query Parameter.Value --output text > /certs/rcx-server.pem
+    else
+        echo "$SERVER_CERT" > /certs/rcx-server.pem
+    fi
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- haproxy "$@"
+fi
+
+if [ "$1" = 'haproxy' ]; then
+	shift # "haproxy"
+	# if the user wants "haproxy", let's add a couple useful flags
+	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
+	#   -db -- disables background mode
+	set -- haproxy -W -db "$@"
+fi
+
+exec "$@"
